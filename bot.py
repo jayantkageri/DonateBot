e51234084d29897f0d80ec9c13bea97f9d9ddb07
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-01-31 19:59:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+#    Donate Bot, A Simple Telegram Bot to tell How to Donate you
+#    Copyright (C) 2021 Jayant Kageri
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 # <! Functions >
 import os
 from telethon import TelegramClient, events, functions, Button
