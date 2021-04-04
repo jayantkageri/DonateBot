bb0b65257025eb68ebafba2952a7af5ee68ea5e3
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-04-04 18:30:31 +0530

    Create Var.py

---
+++ Var.py
@@ -0,0 +1,37 @@
+#    Donate Bot, A Simple Telegram Bot to tell How to Donate you
+#    Copyright (C) 2021 Jayant Kageri
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+ENV = os.environ.get("ENV")
+
+# For Hosing in Heroku, Add your Enviroments in Heroku App Settings
+if ENV:
+  class Var(object):
+    API_ID = int(os.environ.get('API_ID'))
+    APP_HASH = os.environ.get('API_HASH')
+    TOKEN = os.environ.get('TOKEN')
+    OWNER_ID = int(os.environ.get('OWNER_ID'))
+    DONATE_TEXT = os.environ.get(f'DONATE_TEXT')
+
+# For Local Hosing only, Add your Enviroments in the String
+else:
+  class Var(object):
+    API_ID = int("Put you API_ID here (in the String)")
+    API_HASH = "Put your API_HASH here (in the String)
+    TOKEN = "Put your Bot Token here (in the String)"
+    OWNER_ID = int("Put your Telegram ID here (in the String)")
+    DONATE_TEXT = "Put your Donat Text/Message to be repeated here (in the String)"
